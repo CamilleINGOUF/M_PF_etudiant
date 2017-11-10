4c4565d71028279c47a8c33e56e1b717298678f5
CamilleINGOUF <ingouf.camille@gmail.com>
2017-11-10 09:32:44 +0100

    commit

--- TP_haskell/divers.hs
+++ TP_haskell/divers.hs
@@ -1,2 +1,17 @@
+absList::Num a => [a]->[a]
+absList = map (abs)
+
+maxList::Ord a => [a] -> a
+maxList (x:xs) = foldr (max) x xs
+
+first3::[String] -> [String]
+first3 = map (take 3)
+
+short::[String] -> [String]
+short = filter (\x -> (length x) < 3)
+
 main = do
-  print "oui"
+  print $absList [(-10)..10]
+  print $maxList [0..10]
+  print $first3 ["lec","cac","aboetre","netsfyugf","fatgdyu"]
+  print $short ["lec","cac","aboetre","netsfyugf","fatgdyu","ou"]
